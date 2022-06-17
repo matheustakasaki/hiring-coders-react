247fe5d1f9fc9bd563aeddae4d27ea25c4aa88a2
matheustakasaki <matheus8antunes@gmail.com>
2022-06-17 00:58:56 -0300

    Erro no switch

---
+++ gamagit/src/Home.js
@@ -0,0 +1,26 @@
+
+import { useState } from 'react';
+import './App.css';
+import axios from 'axios'
+
+function Home(props) {
+
+  const [usuario, setUsuario] = useState('')
+  function handlePesquisa() {
+
+    axios.get(`https://api.github.com/users/${usuario}/repos`).then(res => console.log(res.data))
+  }
+  
+  return (
+    <div className="App">
+      <header className="App-header">
+
+        <h1>{props.title}, {usuario}!</h1>
+        <input className='usuarioInput' placeholder='Usuário' value={usuario} onChange={e => setUsuario(e.target.value)} />
+        <button type='button' onClick={handlePesquisa}>Pesquisar</button>
+      </header>
+    </div>
+  );
+}
+
+export default Home;
