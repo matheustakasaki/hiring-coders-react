abe500992682f58bd3ed1ee8a5ba865a414c20e1
matheustakasaki <matheus8antunes@gmail.com>
2022-06-16 23:39:29 -0300

    Adicionando estado ao React

--- gamagit/src/App.js
+++ gamagit/src/App.js
@@ -1,10 +1,15 @@
 
+import { useState } from 'react';
 import './App.css';
 
-function App() {
+function App(props) {
+
+  const [usuario, setUsuario] = useState('Taka')
   return (
     <div className="App">
       <header className="App-header">
+
+        <h1>{props.title}, {usuario}!</h1>
         <input name='usuario' id='usuario' className='usuarioInput' placeholder='Usuário' />
       </header>
     </div>
