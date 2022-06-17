35f472369ff2edf3e4d9b53529417ef1e3395d91
matheustakasaki <matheus8antunes@gmail.com>
2022-06-16 23:33:38 -0300

    Inserindo input

--- gamagit/src/App.js
+++ gamagit/src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-
+        <input name='usuario' id='usuario' className='usuarioInput' placeholder='Usuário' />
       </header>
     </div>
   );
