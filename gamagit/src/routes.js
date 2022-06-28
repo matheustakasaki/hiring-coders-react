677bc4baedd3a326d106a003f309ad982c87b9f2
matheustakasaki <matheus8antunes@gmail.com>
2022-06-28 00:08:29 -0300

    Corrigindo versionamento do react router dom

--- gamagit/src/routes.js
+++ gamagit/src/routes.js
@@ -1,16 +1,16 @@
 import React from 'react';
 
-import { Route, Routes, BrowserRouter } from 'react-router-dom';
+import { Route, Switch, BrowserRouter } from "react-router-dom"
 import Repositories from './Repositories';
 import Home from './Home';
 
 export default function Rotas() {
     return (
         <BrowserRouter>
-            <Routes>
-                <Route path='/' exact component={Home} />
-                <Route path='/repositories' component={Repositories} />
-            </Routes>
+            <Switch>
+                <Route path="/" exact component={Home} />
+                <Route component={Repositories} />
+            </Switch>
         </BrowserRouter>
     );
 }
